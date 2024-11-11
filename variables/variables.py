d307cca21c1a19c336ad869dc8620a3a7668096b
Carlos Eduardo <carloseduardo_cmnt@hotmail.com>
2024-11-11 08:53:09 -0300

    Alterações

---
+++ variables/variables.py
@@ -0,0 +1,17 @@
+LOGIN_DATA = {
+    "correctData":{
+        "username":'standard_user',
+        "password":"secret_sauce"
+    },
+    "wrongData":{
+        "username":"cadu79",
+        "password":"1234"
+    }
+}
+
+ERROR_MESSAGES = {
+    "noUsername":"Epic sadface: Username is required",
+    "noCredentials":"Epic sadface: Username is required",
+    "noPassoword":"Epic sadface: Password is required",
+    "wrongCredendials":"Epic sadface: Username and password do not match any user in this service"
+}
